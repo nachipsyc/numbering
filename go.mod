83edace5dc53e8dc76d5cf42e0edeed708d67e31
nachi <scarl.natsu729@gmail.com>
2025-02-16 19:12:32 +0900

    順番指定できるように

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/nachipsyc/numbering
+
+go 1.23.0
+
+require github.com/rwcarlsen/goexif v0.0.0-20190401172101-9e8deecbddbd
--- numbering.go
+++ numbering.go
@@ -7,15 +7,23 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"sort"
+	"time"
+
+	"github.com/rwcarlsen/goexif/exif"
 )
 
 var dir string
 var prefix string
+var sortBy string
+var reverse bool
 
 func main() {
 	// コマンドライン引数を明示
 	dirPtr := flag.String("dir", "", "対象ディレクトリのパス")
 	prefixPtr := flag.String("prefix", "", "共通の接頭辞")
+	sortPtr := flag.String("sort", "name", "ソート順序 (name: 名前順, time: 更新日時順, exif: 撮影日時順)")
+	reversePtr := flag.Bool("reverse", false, "逆順にソート")
 
 	// 入力をパース
 	flag.Parse()
@@ -27,6 +35,8 @@ func main() {
 
 	dir = *dirPtr
 	prefix = *prefixPtr
+	sortBy = *sortPtr
+	reverse = *reversePtr
 
 	// 対象ディレクトリの中の全ファイルを取得([]fs.DirEntry)
 	files, err := getFiles(dir)
@@ -38,7 +48,8 @@ func main() {
 	// 取得したファイルからJPEGファイルのみを抽出([]fs.DirEntry)
 	jpegFiles := extractJpegFiles(files)
 
-	// ファイルをリネーム
+	// ファイルをソートしてからリネーム
+	sortFiles(jpegFiles)
 	renameFiles(jpegFiles)
 }
 
@@ -87,4 +98,62 @@ func renameFiles(jpegFiles []fs.DirEntry) {
 	}
 
 	log.Println("rename completed!!")
+}
+
+// getExifDateTime は写真のExif情報から撮影日時を取得します
+func getExifDateTime(filepath string) (time.Time, error) {
+	f, err := os.Open(filepath)
+	if err != nil {
+		return time.Time{}, err
+	}
+	defer f.Close()
+
+	x, err := exif.Decode(f)
+	if err != nil {
+		return time.Time{}, err
+	}
+
+	return x.DateTime()
+}
+
+// 新しい関数: ファイルのソート
+func sortFiles(files []fs.DirEntry) {
+	// ソート関数を定義
+	var less func(i, j int) bool
+
+	switch sortBy {
+	case "exif":
+		less = func(i, j int) bool {
+			timeI, errI := getExifDateTime(filepath.Join(dir, files[i].Name()))
+			timeJ, errJ := getExifDateTime(filepath.Join(dir, files[j].Name()))
+
+			if errI != nil || errJ != nil {
+				infoI, _ := files[i].Info()
+				infoJ, _ := files[j].Info()
+				return infoI.ModTime().Before(infoJ.ModTime())
+			}
+
+			return timeI.Before(timeJ)
+		}
+	case "time":
+		less = func(i, j int) bool {
+			infoI, _ := files[i].Info()
+			infoJ, _ := files[j].Info()
+			return infoI.ModTime().Before(infoJ.ModTime())
+		}
+	default:
+		less = func(i, j int) bool {
+			return files[i].Name() < files[j].Name()
+		}
+	}
+
+	// reverseが指定されている場合は比較関数を反転
+	if reverse {
+		originalLess := less
+		less = func(i, j int) bool {
+			return !originalLess(i, j)
+		}
+	}
+
+	sort.Slice(files, less)
 }
